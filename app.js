eefd969f7741c2423c4ae6496876a7142b41004d
meonsarkar <80123824+meonsarkar@users.noreply.github.com>
2021-05-15 19:26:41 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,19 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
-};
-
-     
+  var firebaseConfig = {
+    apiKey: "AIzaSyCwf_2vlvF5mtE5OzxlQopqVhBo2AoNvoQ",
+    authDomain: "webapp-e3b03.firebaseapp.com",
+    databaseURL: "https://webapp-e3b03-default-rtdb.firebaseio.com",
+    projectId: "webapp-e3b03",
+    storageBucket: "webapp-e3b03.appspot.com",
+    messagingSenderId: "403137237723",
+    appId: "1:403137237723:web:1323c7a171329fbc2432fe",
+    measurementId: "G-M7HG8P0F2W"
+  };
  
 ////Do Not Edit Below This
 
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
+firebase.analytics();
